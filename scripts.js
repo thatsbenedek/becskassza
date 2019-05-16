1654a235aae9c3bf2b6bdc5d42f33c9247fc9209
Benedek Kozma <33346474+thatsbenedek@users.noreply.github.com>
2019-05-16 23:59:57 +0200

    updated item catalog

--- scripts.js
+++ scripts.js
@@ -2,8 +2,7 @@ const items = [
     {name:'Üveges sör', price:269},
     {name:'Túró rudi', price:99},
     {name:'Tejsüti', price:149},
-    {name:'Kúró Rudi (Xtra large)', price:199},
-    {name:'Arizonás üdítő', price:149}
+    {name:'Üdítő', price:149}
 ];
 
 let balance = 0;
